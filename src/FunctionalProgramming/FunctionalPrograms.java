c5db4e89618f1965a6dbee6f7f5b64c1670e0b41
Priyobrato Das <priyodas12@gmail.com>
2020-05-11 23:20:00 +0530

    orelse

--- src/FunctionalProgramming/FunctionalPrograms.java
+++ src/FunctionalProgramming/FunctionalPrograms.java
@@ -4,8 +4,11 @@ import java.util.Optional;
 
 public class FunctionalPrograms {
     public static void main(String[] args) {
-        Optional<Object> empty=Optional.empty();
+        Optional<String> empty=Optional.of("priyo");
         System.out.println(empty.isEmpty());
         System.out.println(empty.isPresent());
+
+        String orELse=empty.orElse("abdf");
+        System.out.println(orELse);
     }
 }
