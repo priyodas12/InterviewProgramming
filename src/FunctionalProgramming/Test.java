6c24f38e2e9711012c52bfcaa3013e5c90c09fa7
Priyobrato Das <priyodas12@gmail.com>
2020-05-11 23:51:48 +0530

    4

--- src/FunctionalProgramming/Test.java
+++ src/FunctionalProgramming/Test.java
@@ -2,7 +2,7 @@ package FunctionalProgramming;
 
 public class Test {
     public static void main(String[] args) {
-       Student s1= new Student("abcd","ABCD@gmail.com");
-        System.out.println(s1.getEmail().isEmpty());
+       Student s1= new Student("abcd",null);
+        System.out.println(s1.getEmail().map(String::toLowerCase).orElse("Email Not Found"));
     }
 }
