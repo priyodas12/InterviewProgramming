b1fe1d6450b0d91376ecfa325ecd2956b5d6c65c
Priyobrato Das <priyodas12@gmail.com>
2020-05-18 00:02:39 +0530

    added reduce

--- src/Java8/Stream/ListAndStream.java
+++ src/Java8/Stream/ListAndStream.java
@@ -6,17 +6,25 @@ import java.util.stream.Stream;
 
 public class ListAndStream {
     public static void main(String[] args) {
-        List<Integer> list= Arrays.asList(1,2,3,4,5,6,7);
+        List<Integer> list= Arrays.asList(1,2,3,4,5);
 
         //list.stream().forEach(System.out::println);
 
+        //to pick particular element as per condition and do operation.
         //list.stream().filter(e->e>=5).forEach(System.out::println);
 
-        //list.stream().map(e->e*2).forEach(System.out::println);
+        //to pick each element and do operations
+        //list.stream().map(e->e*
+
+        //generating a stream.
         /*Stream<Double> streamGenerated =
                 Stream.generate(() -> Math.random()*100).limit(10);
-        streamGenerated.forEach(System.out::println);*/
+        streamGenerated.forEach(System.out::println);*
+         */
 
+        //get accumulation value
+        int add= list.stream().map(e->e).reduce(0,Integer::sum);
+        System.out.println(add);
 
     }
 }
