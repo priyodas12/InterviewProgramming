2b52bc4cb2624594ee925df5c6df6f55076adb19
Priyobrato Das <priyodas12@gmail.com>
2020-05-18 00:41:30 +0530

    wrong approach

--- src/Java8/Stream/CollectOps.java
+++ src/Java8/Stream/CollectOps.java
@@ -1,5 +1,6 @@
 package Java8.Stream;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CollectOps {
@@ -8,15 +9,36 @@ public class CollectOps {
         return List.of(
                 new Person("abc",20),
                 new Person("bca",91),
-                new Person("bca",9),
-                new Person("bca",18),
-                new Person("bca",13),
-                new Person("bca",7)
+                new Person("bcxa",9),
+                new Person("bcma",18),
+                new Person("bvca",13),
+                new Person("bica",7)
         );
     }
 
     public static void main(String[] args) {
-        String name=createPeople().stream().map(p->p.getName()+" ").reduce("",String::concat);
-        System.out.println(name);
+        //Object oriented programming: polymorphism
+
+        //Functional Programming: functional composition + lazy evaluation(GC problem solved | performance increase)
+
+        //String name=createPeople().stream().map(p->p.getName()+" ").reduce("",String::concat);
+        //System.out.println(name);
+
+        //find out ppl whose age are less than 90
+        //wrong approach
+        /*List<String> persons=new ArrayList<>();
+
+        createPeople()
+                .stream()
+                .filter(p->p.getAge()<90)
+                .map(p->p.getName())
+                .map(String::toUpperCase)
+                .forEach(person->persons.add(person));
+
+        System.out.println(persons);*/
+
+        //right approach
+
+
     }
 }
