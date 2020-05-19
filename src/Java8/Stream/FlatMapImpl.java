a7ccd791f4fae2dd3ac17fd5b86cba12121a840d
Priyobrato Das <priyodas12@gmail.com>
2020-05-19 21:21:37 +0530

    flatmap

--- src/Java8/Stream/FlatMapImpl.java
+++ src/Java8/Stream/FlatMapImpl.java
@@ -1,4 +1,23 @@
 package Java8.Stream;
+/*combination of transforms of values as well as flattening the values
+*
+* map+flattening
+* */
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FlatMapImpl {
+
+    public static void main(String[] args) {
+        List<String> words= Arrays.asList("java","scala","c#","Es6","ruby","pearl");
+
+        //tae each element and process as per function
+        List<Integer> word_length=words.stream().map(i->i.length()).collect(Collectors.toList());
+
+        word_length.forEach(System.out::println);
+
+
+    }
 }
