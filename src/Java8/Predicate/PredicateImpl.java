606f59d1c606cb76681585fc3a181b2ec382c13b
Priyobrato Das <priyodas12@gmail.com>
2020-05-20 21:24:46 +0530

    predicate

---
+++ src/Java8/Predicate/PredicateImpl.java
@@ -0,0 +1,13 @@
+package Java8.Predicate;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+//predicate is a functional interface which having single abstract method test(); return boolean
+public class PredicateImpl {
+
+    //filter always checks boolean condition:
+    public static void main(String[] args) {
+        System.out.println(Arrays.asList(1,2,3,4,7,5,9,6).stream().filter(i->i%3==0).collect(Collectors.toList()));
+    }
+}
