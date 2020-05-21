630d9ddecb6bccdcc1b47a8e332f79735d904013
Priyobrato Das <priyodas12@gmail.com>
2020-05-21 12:35:37 +0530

    chain function

--- src/Java8/Function/FunctionChainImpl.java
+++ src/Java8/Function/FunctionChainImpl.java
@@ -1,5 +1,9 @@
 package Java8.Function;
 
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /*
 * if two Function f1,f2 is there
 *
@@ -8,4 +12,11 @@ package Java8.Function;
 * f1.compose(f2) -->f2 will be applied first before f1;
 * */
 public class FunctionChainImpl {
+    public static void main(String[] args) {
+
+        Function<String,String> f1=s->s.toUpperCase();
+        Function<String,String> f2=s->s.substring(1,3);
+
+        System.out.println(Arrays.asList("abcdss","deffg","kdsdas").stream().map(f1.andThen(f2)).collect(Collectors.toList()));
+    }
 }
