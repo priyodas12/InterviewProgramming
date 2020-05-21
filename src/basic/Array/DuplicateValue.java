28cc7cc91d6fd94a7d82a3c2dc730c27d5d47b6b
Priyobrato Das <priyodas12@gmail.com>
2020-05-22 01:35:12 +0530

    array

---
+++ src/basic/Array/DuplicateValue.java
@@ -0,0 +1,7 @@
+package basic.Array;
+
+public class DuplicateValue {
+    public static void main(String[] args) {
+        Integer [] numbers= new Integer[]{3,1,4,2,8,0};
+    }
+}
---
+++ src/basic/Array/FindElement.java
@@ -0,0 +1,13 @@
+package basic.Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindElement {
+    public static void main(String[] args) {
+        Integer [] numbers= new Integer[]{3,1,4,2,8,0};
+
+        System.out.println(Arrays.asList(numbers).contains(10));
+    }
+}
