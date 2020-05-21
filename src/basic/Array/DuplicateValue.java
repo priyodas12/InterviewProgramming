ff9fdd15e864184dd46a9d8b5377ad41e60f584c
Priyobrato Das <priyodas12@gmail.com>
2020-05-22 01:57:54 +0530

    array

--- src/basic/Array/DuplicateValue.java
+++ src/basic/Array/DuplicateValue.java
@@ -1,7 +1,11 @@
 package basic.Array;
 
+import java.util.Arrays;
+
 public class DuplicateValue {
     public static void main(String[] args) {
-        Integer [] numbers= new Integer[]{3,1,4,2,8,0};
+        Integer [] numbers= new Integer[]{3,1,4,2,8,0,4,0};
+
+        Arrays.asList(numbers).stream().filter(i->i%2==0);
     }
 }
